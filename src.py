0334978ee99c54767cde72b7fea4b9bfc06f651e
MalesSebastian <malessebastian@gmail.com>
2017-08-23 14:46:49 +0300

    Fit hour for every plot

--- src.py
+++ src.py
@@ -46,31 +46,25 @@ sums = time_rdd.reduce(add)
 seconds = sums / time_rdd.count()
 
 # Calculate the busiest hours
-hours = fullset.flatMap(lambda line: (line.split(",")[2].split(" ")[1],
-                                      line.split(",")[5].split(" ")[1])).cache()
-hours_pair = hours.map(lambda line: (line.split(":")[0], 1))
+hours_pair = fullset.map(lambda line: (fit_hour(line), 1))
 hours_results = hours_pair.reduceByKey(lambda a, b: a + b).sortBy(lambda a:
                                                                   a[1]).cache()
 
 # Calculate the busiest hours on weekend days
 date_and_hours_for_weekend = fullset.filter(lambda line: check_weekend(line))
-hours_for_weekend = date_and_hours_for_weekend.flatMap(
-    lambda line: (
-        line.split(",")[2].split(" ")[1],
-        line.split(",")[5].split(" ")[1]))
-hours_pair_weekend = hours_for_weekend.map(
-    lambda line: (line.split(":")[0], 1))
+hours_pair_weekend = date_and_hours_for_weekend.map(
+    lambda line: (fit_hour(line), 1))
 hours_results_weekend = hours_pair_weekend.reduceByKey(
     lambda a, b: a + b).sortBy(lambda a: a[1]).cache()
 
 # Calculate the hours with the longes rides
 hour_and_duration = fullset.map(lambda line: (fit_hour(line),
-                                              line.split(",")[1]))
+                                              int(line.split(",")[1])))
 hour_and_durations_sumsav = hour_and_duration.aggregateByKey(
-                            (0, 0),
-                            lambda a, b: (a[0] + int(b), a[1] + 1),
-                            lambda a, b: (a[0] + b[0], a[1] + b[1]))
-averages = hour_and_durations_sumsav.mapValues(lambda x: x[0] / x[1])
+    (0, 0), lambda a, b: (a[0] + b, a[1] + 1),
+    lambda a, b: (a[0] + b[0], a[1] + b[1])
+)
+averages = hour_and_durations_sumsav.mapValues(lambda x: x[0] / x[1]).cache()
 
 # Plot data
 trace1 = go.Bar(
